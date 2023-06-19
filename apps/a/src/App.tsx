93b64727fd8007f8886a483090573b785fb4ca6a
h-ito <132866096+etm7fp@users.noreply.github.com>
2023-06-19 16:10:57 +0900

    chore(a): update counter

--- apps/a/src/App.tsx
+++ apps/a/src/App.tsx
@@ -1,10 +1,11 @@
+import { double } from '@example-monorepo/utils'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <>
@@ -18,8 +19,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => double(count))}>
+          doubling {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
