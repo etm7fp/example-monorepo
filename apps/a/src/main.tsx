51ddddadb59a6e82b32d9ad31d611a8e7d43e8d7
h-ito <132866096+etm7fp@users.noreply.github.com>
2023-06-20 12:22:32 +0900

    chore(a): setup mocks

--- apps/a/src/main.tsx
+++ apps/a/src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+/** @see https://github.com/mswjs/msw/discussions/712#discussioncomment-1080752 */
+async function prepare(): Promise<void> {
+  if (import.meta.env.DEV) {
+    const { worker } = await import('@example-monorepo/mocks/browser')
+    worker.start()
+  }
+}
+
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
