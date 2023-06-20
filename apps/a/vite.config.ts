3896ff21c8a1019d1ca54d05d4e01a52141605a5
h-ito <132866096+etm7fp@users.noreply.github.com>
2023-06-20 12:58:25 +0900

    chore(a): add stripMockServiceWorker plugin

--- apps/a/vite.config.ts
+++ apps/a/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+import { defineConfig, type PluginOption } from 'vite'
+
+/** @see https://github.com/vitejs/vite/discussions/7374#discussioncomment-5887382 */
+function stripMockServiceWorker(): PluginOption {
+  return {
+    name: 'strip-mock-service-worker',
+    resolveId(source) {
+      return source === 'virtual-module' ? source : null
+    },
+    writeBundle(outputOptions, _inputOptions) {
+      const outDir = outputOptions.dir
+      const filePath = path.resolve(outDir, 'mockServiceWorker.js')
+      fs.unlink(filePath, () => console.log(`Deleted ${filePath}`))
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), stripMockServiceWorker()],
 })
